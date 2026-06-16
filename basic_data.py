b31d11ad90c335ffa8f0205cc682094f36504eac
KairiCL <benedictfusin99@gmail.com>
2026-06-16 13:38:39 +0800

    Create basic_data.py with variable examples
    Added variables and data types with examples.

---
+++ basic_data.py
@@ -0,0 +1,18 @@
+#Variables and data types
+#The equal sign "=" is used to assign data and values.
+#Strings: (str). Data in texts, always enclosed in quotes.
+
+name = "Kai"
+
+#Integers: (int). Values that are in whole numbers.
+age = 28
+
+#Floats: (float). Values that are in decimal. Most likely used for precise data.
+temperature = 98.6
+
+#Booleans: (bool). True or false values. Most likely used to filter the data.
+is_active = True
+
+print(name)
+print(age)
+print(temperature)
